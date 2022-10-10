dc39fa3c919c1f6de4329d3231068eaff2ed9e10
Safiyy Kanjiyani <repulsivechannel@Gmail.com>
2022-10-10 12:07:28 -0700

    health check !

--- web.py
+++ web.py
@@ -2,7 +2,7 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 import time
 
 hostName = "localhost"
-serverPort = 8080
+serverPort = 10000
 
 class MyServer(BaseHTTPRequestHandler):
     def do_GET(self):
